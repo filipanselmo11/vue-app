13be658478e2ec9a618466af4edc643573532569
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2022-06-17 13:35:49 -0400

    monitoramento com watchers

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,64 @@
 <template>
   <div id="app">
-    <button>Click</button>
+    <input type="text" v-model="text.question" />
+    <span>Text: {{ text }}</span>
+    <span>Api: {{ apiResponse }}</span>
   </div>
 </template>
 
 <script>
+/*eslint-disable*/
 export default {
   name: "App",
   data: () => ({
-    count: {
-      x: 0,
+    text: {
+      question: "",
     },
+    // text: "",
+    apiResponse: "Start",
   }),
-  mounted() {
-    // this.y = 0;
-    // this.$set(this.count, "y", 1);
-    this.count = { ...this.count, ...{ a: 1, b: 2 } };
+  methods: {
+    simulateApi(response, time) {
+      return new Promise((res) => {
+        setTimeout(() => {
+          this.apiResponse = response;
+          console.log("api promisse");
+        }, 1000 * time);
+      });
+    },
+    async fetchApi(value, time) {
+      await this.simulateApi(value, time);
+    },
+    callApi(newValue, oldValue) {
+      console.log(newValue, oldValue);
+      this.fetchApi("Call", 1);
+    },
+  },
+  watch: {
+    "text.question"(val, old) {
+      console.log(val, old);
+    },
+    // text: [
+    //   "callApi",
+    //   function handler2(val, old) {
+    //     console.log(val, old);
+    //     this.fetchApi("Handler 2", 2);
+    //   },
+    //   {
+    //     handler: function handler3(val, old) {
+    //       console.log(val, old);
+    //       this.fetchApi("Handler 3", 3);
+    //     },
+    //   },
+    // ],
+    // text(newValue, oldValue) {},
+    // text: "callApi",
+    // text: {
+    //   immediate: true,
+    //   handler(newValue, oldValue) {
+    //     this.fetchApi(Math.random());
+    //   },
+    // },
   },
 };
 </script>
