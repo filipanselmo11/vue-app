d8c4613ee0217271efd885c7ca19112c736b71ca
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2022-06-17 12:19:47 -0400

    ASdasdasd

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button>Click</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: () => ({
+    count: {
+      x: 0,
+    },
+  }),
+  mounted() {
+    // this.y = 0;
+    // this.$set(this.count, "y", 1);
+    this.count = { ...this.count, ...{ a: 1, b: 2 } };
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
 }
 </style>
